987e87ab14bccd4f38d46872fa9b8df76805302d
gevorg.bagratunyan <gevorg.bagratunyan@softr.io>
2022-05-01 18:07:22 +0400

    Tests for AC1

--- src/test/java/com/glc/AppTest.java
+++ src/test/java/com/glc/AppTest.java
@@ -1,5 +1,6 @@
 package com.glc;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,18 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    //Given that I visit the site, when I first start, I expect my list to be empty.
+    @Test
+    public void onFirstCallBookListShouldBeEmpty() {
+        //SETUP
+        ReadingList readingList = new ReadingList();
+
+        //EXECUTE
+        List<Book> books = readingList.getBookList();
+
+        //ASSERT
+        assertEquals(books.size(), 0);
     }
 }
