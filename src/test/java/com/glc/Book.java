45c5eeacbc55a5886db854cdf0487277d93f1d3e
gevorg.bagratunyan <gevorg.bagratunyan@softr.io>
2022-05-01 18:53:35 +0400

    Code for AC2

--- src/test/java/com/glc/Book.java
+++ src/test/java/com/glc/Book.java
@@ -1,4 +1,16 @@
 package com.glc;
 
 public class Book {
+
+    private String title;
+    private String author;
+    private int length;
+    private int year;
+
+    public Book(String title, String author, int length, int year) {
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.year = year;
+    }
 }
--- src/test/java/com/glc/ReadingList.java
+++ src/test/java/com/glc/ReadingList.java
@@ -2,12 +2,39 @@ package com.glc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReadingList {
 
-    private List<Book> books = new ArrayList<>();
+    private class BookItems {
+        Book book;
+        String readDate;
+        int rating;
+
+        public BookItems(Book book, String readDate, int rating) {
+            this.book = book;
+            this.readDate = readDate;
+            this.rating = rating;
+        }
+    }
+
+    private List<BookItems> books = new ArrayList<>();
 
     public List<Book> getBooks() {
-        return books;
+        return books.stream()
+                .map(bookItems -> bookItems.book)
+                .collect(Collectors.toList());
+    }
+
+    public void addBook(Book book, String readDate, int rating) {
+        if (rating > 5 || rating < 1) {
+            throw new RuntimeException("Rating should be between 1 and 5");
+        }
+        books.add(new BookItems(book, readDate, rating));
+    }
+
+
+    public int readNumber() {
+        return books.size();
     }
 }
